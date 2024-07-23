bf953b48a1bf497eb1cebc19f8fc03d32646c061
Charity Wekesa <168071215+PoliteGreenCow@users.noreply.github.com>
2024-07-23 21:41:10 +0930

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,32 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
-import { sampleProducts } from './data';
+import { Container, Nav , Navbar} from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <header>TS Amazona</header>
+    <div className='d-flex flex-column vh-100'>
+      <header>
+        <Navbar bg ="dark" variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand>ts-ecommerce</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className='nav-link'>Cart</a>
+            <a href="/signin" className='nav-link'> SignIn</a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {sampleProducts.map((product) => (
-            <li key={product.slug}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="product-image"
-              />
-              <h2>{product.name}</h2>
-              <p>${product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className='mt-3'>
+          <Outlet/>
+        </Container>        
       </main>
-      <footer>All rights reserved</footer>
+      <footer>
+        <div className='text-center'>
+        All rights reserved
+        </div>
+        </footer>
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
+import './index.css'
+import HomePage from './pages/Homepage'
+import ProductPage from './pages/Productpage'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomePage />} />
+      <Route path="product/:slug" element={<ProductPage />} />
+      {/* <Route path="dashboard" element={<Dashboard />} /> */}
+      {/* ... etc. */}
+    </Route>
+  )
+)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>    
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
