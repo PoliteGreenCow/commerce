a83a158276a847758a8d7f4ab9eba12f494fad12
Charity Wekesa <168071215+PoliteGreenCow@users.noreply.github.com>
2024-07-25 04:40:51 +0930

    another one

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
-import React, { useContext, useEffect } from 'react';
-import { Container, Nav, Navbar, Button } from 'react-bootstrap'; // Import Button
-import { Outlet } from 'react-router-dom';
-import logo from "../public/images/logo.png"; // Make sure the path to the logo is correct
+import { useContext, useEffect } from 'react';
+import { Container, Nav, Navbar, Button, Badge } from 'react-bootstrap';
+import { Outlet, Link } from 'react-router-dom';
+import CartImg from '../public/images/cart_icon.png';
+import logo from '../public/images/logo.png';
+import { LinkContainer } from 'react-router-bootstrap';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { Store } from './Store';
 
 const App = () => {
   const {
-    state: { mode },
+    state: { mode, cart },
     dispatch,
   } = useContext(Store);
 
@@ -19,46 +23,56 @@ const App = () => {
   };
 
   return (
-    <div className='d-flex flex-column vh-100'>
+    <div className="d-flex flex-column vh-100">
+      <ToastContainer position='bottom-center' limit={1}/>
       <header>
-        <Navbar expand='lg'>
+        <Navbar expand="lg">
           <Container>
-            <Navbar.Brand href="/">
-              <img 
-                src={logo} 
-                alt="Shoppy Logo" 
-                height="45" 
+            <LinkContainer to="/">
+            <Navbar.Brand as={Link} to="/">
+              <img
+                src={logo}
+                alt="Shoppy Logo"
+                height="45"
                 className="d-inline-block align-top me-2"
               />
               Shoppy
             </Navbar.Brand>
+            </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                <Button variant={mode} onClick={switchModeHandler}>
+              <Nav className="ms-auto align-items-center">
+                <Button variant={mode} onClick={switchModeHandler} className="me-2">
                   <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
                 </Button>
-                <Nav.Link href="/cart">Cart</Nav.Link>
-                <Nav.Link href="/signin">Sign In</Nav.Link>
+                <Nav.Link as={Link} to="/cart" className="d-flex align-items-center">
+                  <img src={CartImg} alt="Cart" className="me-1" />
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Nav.Link>
+                <Nav.Link as={Link} to="/signin">Sign In</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
       </header>
       <main className="flex-grow-1">
-        <Container className='mt-3'>
+        <Container className="mt-3">
           <Outlet />
         </Container>
       </main>
       <footer className="py-3 bg-light">
         <Container>
-          <div className='text-center'>
+          <div className="text-center">
             All rights reserved
           </div>
         </Container>
       </footer>
     </div>
   );
-}
+};
 
 export default App;
